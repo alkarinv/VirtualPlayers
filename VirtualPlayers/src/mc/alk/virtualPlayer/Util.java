aaccd2911a091ff46485b06a1fe28011712c5e06
MCForger <mrseanmcd@gmail.com>
2013-03-25 16:13:23 -0400

    f - Adding VirtualPlayer moving functions.

--- VirtualPlayers/src/mc/alk/virtualPlayer/Util.java
+++ VirtualPlayers/src/mc/alk/virtualPlayer/Util.java
@@ -3,30 +3,36 @@ package mc.alk.virtualPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
-public class Util {
+public class Util
+{
 
-	static public String getLocString(Location l){
-		if (l == null)
-			return "null";
+	static public String getLocString(Location l)
+	{
+		if (l == null) return "null";
 		StringBuilder sb = new StringBuilder();
-		sb.append( ((isInt(l.getX())) ? l.getBlockX() : l.getX()) + ",") ;
-		sb.append( ((isInt(l.getY())) ? l.getBlockY() : l.getY()) + ",") ;
-		sb.append( ((isInt(l.getZ())) ? l.getBlockZ() : l.getZ())) ;
+		sb.append(((isInt(l.getX())) ? l.getBlockX() : l.getX()) + ",");
+		sb.append(((isInt(l.getY())) ? l.getBlockY() : l.getY()) + ",");
+		sb.append(((isInt(l.getZ())) ? l.getBlockZ() : l.getZ()));
 		if (l.getYaw() != 0f) sb.append("," + l.getYaw());
 		if (l.getPitch() != 0f) sb.append("," + l.getPitch());
 		return sb.toString();
 	}
 
-	public static void sendMessage(VirtualPlayer p, String msg){
-		final String finalMsg = colorChat("&5["+p.getDisplayName()+"] &b'" + msg+"'");
+	public static void sendMessage(VirtualPlayer p, String msg)
+	{
+		final String finalMsg = colorChat("&5[" + p.getDisplayName() + "] &b'"
+				+ msg + "'");
 		Bukkit.getConsoleSender().sendMessage(finalMsg);
-		if (p.getInformed() != null)
-			p.getInformed().sendMessage(finalMsg);
+		if (p.getInformed() != null) p.getInformed().sendMessage(finalMsg);
 	}
 
-	public static String colorChat(String msg) {return msg.replace('&', (char) 167);}
+	public static String colorChat(String msg)
+	{
+		return msg.replace('&', (char) 167);
+	}
 
-	public static boolean isInt(double f) {
+	public static boolean isInt(double f)
+	{
 		return Math.floor(f) == f;
 	}
 
