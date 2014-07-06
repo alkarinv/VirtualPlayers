4e89bfc5efe369b21d5209386a6a1366b01e93d9
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-07-05 21:28:28 -0500

    Added backwards compatibility.
    Tested against craftbukkit 1.2.5 to 1.7.9
    
    All v1_X_RY imports and the corresponding code was moved out of the
    VirtualPlayers JavaPlugin class.
    
    Most methods were moved into
    mc.alk.virtualplayers.nms.v1_X_RY.VirtualPlayer.java
    
    The listener methods were moved into
    mc.alk.virtualplayers.nms.v1_X_RY.PlayerListener.java
    
    API methods implemented:
    setEventMessages(boolean enable);
    setPlayerMessages(boolean show);
    
    onDisable() functionality:
    mc.alk.virtualplayers.nms.{version}.VirtualPlayer.deleteVirtualPlayers();
    
    Finally, some classes still need to be refactored out of the nms packages
    and converted into version independent classes.

---
+++ modules/v1_5_R3/src/main/java/mc/alk/virtualplayers/nms/v1_5_R3/VPBaseExecutor.java
@@ -0,0 +1,115 @@
+package mc.alk.virtualplayers.nms.v1_5_R3;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author alkarin
+ */
+public class VPBaseExecutor extends CustomCommandExecutor{
+    static boolean showTransitionsToPlayer = true;
+    static boolean showEventMessages = true;
+
+    protected VPBaseExecutor(Plugin plugin) {
+        super(plugin);
+    }
+
+    public VPBaseExecutor(Plugin plugin, int i) {
+        super(plugin, i);
+    }
+
+    @Override
+    protected String getUsageString(Class<?> clazz) {
+        if (VirtualPlayer.class == clazz){
+            return "<vp>";
+        }
+        return super.getUsageString(clazz);
+    }
+
+    @Override
+    protected Object verifyArg(CommandSender sender, Class<?> clazz, Command command, String[] args,int curIndex, AtomicInteger numUsedStrings) {
+        if (VirtualPlayer.class == clazz){
+            return verifyVirtualPlayer(args[curIndex], numUsedStrings);
+        }
+        return super.verifyArg(sender, clazz, command, args, curIndex, numUsedStrings);
+    }
+
+    private Object verifyVirtualPlayer(String name, AtomicInteger numUsedStrings) {
+        Player p = VirtualPlayer.getPlayerExact(name);
+        if (p == null) {
+            if (name.contains("-")) { /// return range
+                return getVps(name);}
+            p = VirtualPlayer.getOrCreate(name);
+        }
+        if (!(p instanceof VirtualPlayer)){
+            throw new IllegalArgumentException("Player " + name + " is not a VirtualPlayer");
+        }
+        numUsedStrings.set(1);
+        return p;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected Object invoke(MethodWrapper mwrapper, Arguments args)
+            throws InvocationTargetException, IllegalAccessException {
+        if (args.args.length > 1 && args.args[1] instanceof List ) {
+            Object ret = null;
+            Object[] o = Arrays.copyOfRange((Object[]) args.args[2], 1, ((Object[])args.args[2]).length);
+            args.args[2] = o;
+            List<VirtualPlayer> list = (List<VirtualPlayer>) args.args[1];
+            for (Object vp : list){
+                args.args[1] = vp;
+                ret = mwrapper.method.invoke(mwrapper.obj, args.args);
+            }
+            return ret;
+        } else {
+            return mwrapper.method.invoke(mwrapper.obj, args.args);
+        }
+    }
+
+    @SuppressWarnings("SimplifiableIfStatement")
+    @Override
+    public boolean sendMessage(CommandSender sender, String string)
+    {
+        if (string == null || !showEventMessages) return true;
+        return super.sendMessage(sender, string);
+    }
+
+    protected boolean sendMessage(CommandSender sender, VirtualPlayer p,  String string)
+    {
+        if (string == null || !showEventMessages) return true;
+        if (showTransitionsToPlayer)
+            Util.sendMessage(p, string);
+        return super.sendMessage(sender,string);
+    }
+
+
+    private List<VirtualPlayer> getVps(String names) {
+        String name = names;
+        List<VirtualPlayer> players = new ArrayList<VirtualPlayer>();
+        if (names.contains("-")){
+            String[] split = names.split("-");
+            name = split[0];
+            Integer start = Integer.valueOf(split[1]);
+            Integer end = Integer.valueOf(split[2]);
+            for (int i=start;i<=end;i++){
+                players.add(VirtualPlayer.getOrCreate(name + i));
+            }
+        } else {
+            players.add(VirtualPlayer.getOrCreate(name));
+        }
+        return players;
+    }
+
+    public static void setShowEventMessages(boolean show) {
+        showEventMessages = show;
+    }
+}
---
+++ modules/vpre/src/main/java/mc/alk/virtualplayers/nms/vpre/PlayerListener.java
@@ -0,0 +1,55 @@
+package mc.alk.virtualplayers.nms.vpre;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+/**
+ *
+ * @author alkarin
+ */
+public class PlayerListener implements Listener {
+    
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerRespawnBegin(PlayerRespawnEvent event){
+        Player p = event.getPlayer();
+        if (VirtualPlayer.getVps().containsKey(p.getUniqueId()) && (p instanceof VirtualPlayer))
+        {
+            p.setHealth(20);
+            p.setLastDamageCause(null);
+            ((VirtualPlayer) p).setOnline(true);
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerRespawnEnd(PlayerRespawnEvent event)
+    {
+        Player p = event.getPlayer();
+        if (VirtualPlayer.getVps().containsKey(p.getUniqueId()) && (p instanceof VirtualPlayer))
+        {
+            final Location l = event.getRespawnLocation();
+            ((VirtualPlayer) p).teleport(l, true);
+        }
+    }
+    
+    @EventHandler
+    public void onAsyncChatEvent(AsyncPlayerChatEvent event)
+    {
+        if (!VirtualPlayer.getVps().containsKey(event.getPlayer().getUniqueId())) // / don't need to
+            // handle it
+            return;
+        // / For some reason we do need to actually send the messages from
+        // virtualplayers ourself
+        final String message = String.format(event.getFormat(), event
+                .getPlayer().getDisplayName(), event.getMessage());
+        for (Player p : event.getRecipients())
+        {
+            p.sendMessage(message);
+        }
+    }
+    
+}
