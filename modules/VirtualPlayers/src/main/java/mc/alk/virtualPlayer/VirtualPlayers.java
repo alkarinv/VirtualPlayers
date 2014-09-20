26fbb8a36b1ebde65e867887a71d26e8ba332918
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-09-20 16:17:53 -0500

    Forwards any calls to the new package/class.
    The package structure has changed to a more standarized syntax/notation of
    all lowercase, and the project name. The new package structure also
    contains versioned packages.
    
    old: mc.alk.virtualPlayer;
    new: mc.alk.virtualplayers;
    
    Any static method calls made to mc.alk.virtualPlayer.VirtualPlayers:* is
    forwarded to the new class at mc.alk.virtualplayers.VirtualPlayers:*
    
    Originally, when I made the change, I didn't realize that other plugins
    were hooking into the VirtualPlayers code base.
    
    I noticed that mc.alk.arena.util.ServerUtil is making such calls. Not sure
    if anyone else is hooking into VirtualPlayers. But this mapping of the old
    class to the new class will allow plugins to use newer versions of
    VirtualPlayers without breaking and without recompiling.

---
+++ modules/VirtualPlayers/src/main/java/mc/alk/virtualPlayer/VirtualPlayers.java
@@ -0,0 +1,114 @@
+package mc.alk.virtualPlayer;
+
+import java.util.List;
+import java.util.Map;
+import org.bukkit.entity.Player;
+
+/**
+ * This class maps the old package to the new package. <br/><br/>
+ * <pre>
+ * old package: mc.alk.virtualPlayer;
+ * new package: mc.alk.virtualplayers;
+ * 
+ * So that old plugins don't have to update to the new package structure.
+ * 
+ * The primary reason for the change is that several of these methods 
+ * contained version dependent code. So they needed to be put into a 
+ * versioned package structure.
+ * </pre>
+ * @author Nikolai
+ */
+public class VirtualPlayers {
+    
+    /**
+     * Used by mc.alk.arena.util.ServerUtil.
+     * @param pname
+     * @return
+     * @deprecated
+     */
+    @Deprecated
+    public static Player getPlayer(String pname)
+    {
+        return mc.alk.virtualplayers.VirtualPlayers.getPlayer(pname);
+    }
+
+    @Deprecated
+    public static Player getPlayerExact(String pname)
+    {
+        return mc.alk.virtualplayers.VirtualPlayers.getPlayerExact(pname);
+    }
+
+    @Deprecated
+    public static Player getOrMakePlayer(String pname)
+    {
+        return mc.alk.virtualplayers.VirtualPlayers.getOrMakePlayer(pname);
+    }
+
+    @Deprecated
+    public static Player[] getOnlinePlayers()
+    {
+        return mc.alk.virtualplayers.VirtualPlayers.getOnlinePlayers();
+    }
+
+    @Deprecated
+    public static Player makeVirtualPlayer() throws Exception {
+        return makeVirtualPlayer(null);
+    }
+
+    @Deprecated
+    public static synchronized Player makeVirtualPlayer(String name) throws Exception
+    {
+        return mc.alk.virtualplayers.VirtualPlayers.makeVirtualPlayer(name);
+    }
+
+    /**
+     * Object parameter needs to be changed to a VirtualPlayer interface.
+     * @param vp Pass in an instance of VirtualPlayer.
+     * @return 
+     */
+    @Deprecated
+    public static Player deleteVirtualPlayer(Object vp)
+    {
+        return mc.alk.virtualplayers.VirtualPlayers.deleteVirtualPlayer(vp);
+    }
+
+    @Deprecated
+    public static void deleteVirtualPlayers()
+    {
+        mc.alk.virtualplayers.VirtualPlayers.deleteVirtualPlayers();
+    }
+
+    /**
+     * Moved to mc.alk.virtualplayers.VirtualPlayers:getPlayerList().
+     * @return <pre> {@literal List<VirtualPlayer> } </pre>
+     */
+    @Deprecated
+    public static List getPlayerList() {
+        return (List) mc.alk.virtualplayers.VirtualPlayers.getPlayerList();
+    }
+    
+    @Deprecated
+    public static Object getOrCreate(String name) {
+        return mc.alk.virtualplayers.VirtualPlayers.getOrCreate(name);
+    }
+    
+    /**
+     * Moved to mc.alk.virtualplayers.VirtualPlayers:getVps().
+     * @return <pre> {@literal Map<UUID, VirtualPlayer> } </pre>
+     * @deprecated
+     */
+    @Deprecated
+    public static Map getVps() {
+        return (Map) mc.alk.virtualplayers.VirtualPlayers.getVps();
+    }
+    
+    /**
+     * Moved to mc.alk.virtualplayers.VirtualPlayers:getNames().
+     * @return <pre> {@literal Map<String, VirtualPlayer> } </pre>
+     * @deprecated
+     */
+    @Deprecated
+    public static Map getNames() {
+        return (Map) mc.alk.virtualplayers.VirtualPlayers.getNames();
+    }
+}
