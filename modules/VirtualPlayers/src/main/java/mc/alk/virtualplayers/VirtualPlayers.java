7f1a96c3dcb64797691b9566438d48de8810eb35
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-09-20 16:30:22 -0500

    Re-added old methods that forward calls to the nms package.
    These methods were originally moved to
    mc.alk.virtualplayers.nms.{version}.VirtualPlayer
    
    because they contained version dependent code.
    
    The methods were re-added because mc.alk.arena.util.ServerUtil uses them.
    
    These new methods simply use reflection to call the correct VirtualPlayer
    class depending on what Minecraft version the server is running:
    
    mc.alk.virtualplayers.nms.{version}.VirtualPlayer.class
    
    This was not tested, but it should work. A unit test was written for the
    invoke(String methodName, Object param) method.
    
    The test simply ensures that the correct type of the Object param can be
    obtained by the JVM.
    
    Also, some of these methods contained references to
    {version}.VirtualPlayer.
    
    All VirtualPlayer types were changed to Object.
    
    Ultimately, references should be made via some kind of VirtualPlayer
    interace. But the problem is that getHeatlh() returns an int for older
    Minecraft versions. And getHealth() returns float/double for newer
    Minecraft versions.

--- modules/VirtualPlayers/src/main/java/mc/alk/virtualplayers/VirtualPlayers.java
+++ modules/VirtualPlayers/src/main/java/mc/alk/virtualplayers/VirtualPlayers.java
@@ -1,25 +1,30 @@
 package mc.alk.virtualplayers;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import mc.alk.virtualplayers.util.Version;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class VirtualPlayers extends JavaPlugin implements Listener
-{
-    Version server;
+public class VirtualPlayers extends JavaPlugin {
+    
+    private Version server;
     public static final String MAX = "1.7.10-R9.9-SNAPSHOT";
     public static final String MIN = "1.2.5";
-    String NMS;
+    public static final String NMS = Version.getNmsVersion().toString();
 
     @Override
     public void onEnable() {
-        NMS = Version.getNmsVersion().toString();
         server = Version.getServerVersion();
         if (!server.isSupported(MAX) || !server.isCompatible(MIN)) {
             getLogger().info("VirtualPlayers is not compatible with your server.");
@@ -64,7 +69,7 @@ public class VirtualPlayers extends JavaPlugin implements Listener
         }
     }
 
-    public Class<?> getNmsClass(String clazz) throws Exception {
+    public static Class<?> getNmsClass(String clazz) throws Exception {
         return Class.forName("mc.alk.virtualplayers.nms." + NMS + "." + clazz);
     }
 
@@ -91,6 +96,150 @@ public class VirtualPlayers extends JavaPlugin implements Listener
         } catch (Exception ex) {
             Logger.getLogger(VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    /**
+     * Used by mc.alk.arena.util.ServerUtil.
+     * Implementation moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer:
+     */
+    public static Player getPlayer(String pname)  {
+        return (Player) invoke("getPlayer", pname);
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Player getPlayer(UUID id)  {
+        return (Player) invoke("getPlayer", id);
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Player getPlayerExact(String pname)  {
+        return (Player) invoke("getPlayerExact", pname);
+    }
+
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Player getOrMakePlayer(String pname)  {
+        return (Player) invoke("getOrMakePlayer", pname);
+    }
+    
+    /**
+     * Used by mc.alk.arena.util.ServerUtil.
+     * Implementation moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer:getOnlinePlayers().
+     */
+    public static Player[] getOnlinePlayers()  {
+        return (Player[]) invoke("getOnlinePlayers");
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Player makeVirtualPlayer() throws Exception {
+        return makeVirtualPlayer(null);
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static synchronized Player makeVirtualPlayer(String name) throws Exception {
+        return (Player) invoke("makeVirtualPlayer", name);
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Player deleteVirtualPlayer(Object vp)  {
+        Player player = null;
+        Method[] methods = null;
+        try {
+            methods = getNmsClass("VirtualPlayer").getDeclaredMethods();
+        } catch (Exception ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (Method m : methods) {
+            Class<?>[] params = m.getParameterTypes();
+            if (m.getName().equalsIgnoreCase("deleteVirtualPlayer")) {
+                try {
+                    player = (Player) m.invoke(null, vp);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalArgumentException ex) {
+                    Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InvocationTargetException ex) {
+                    Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return player;
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static void deleteVirtualPlayers() {
+        invoke("deleteVirtualPlayers");
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static List getPlayerList() {
+        return (List) invoke("getPlayerList");
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Object getOrCreate(String name)  {
+        return invoke("getOrCreate", name);
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Map getVps()  {
+        return (Map) invoke("getVps");
+    }
+    
+    /**
+     * Method moved to mc.alk.virtualplayers.nms.{version}.VirtualPlayer.
+     */
+    public static Map getNames()  {
+        return (Map) invoke("getNames");
+    }
+    
+    private static Object invoke(String methodName, Object... params) {
+        Object object = null;
+        Method method;
+        try {
+            if (params == null) {
+                method = getNmsClass("VirtualPlayer").getDeclaredMethod(methodName);
+            } else {
+                Class[] classParams = new Class[params.length];
+                for (int index = 0; index < params.length; index = index + 1) {
+                    classParams[index] = params[index].getClass();
+                }
+                method = getNmsClass("VirtualPlayer").getDeclaredMethod(methodName, classParams);
+            }
+            object = method.invoke(null, params);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(mc.alk.virtualPlayer.VirtualPlayers.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return object;
     }
 
 }
